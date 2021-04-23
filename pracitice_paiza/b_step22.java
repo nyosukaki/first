e4d32649e1b02b779ba65fb62eed893e2f5f5052
新垣輝 <arakakihikaru@MacBook-Pro-5.local>
2021-04-24 08:09:13 +0900

    文字列を大文字にするメソッド_toUpperCase

---
+++ pracitice_paiza/b_step22.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+       
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        
+        // 文字列を大文字にするメソッド_toUpperCase()
+        System.out.println(line.toUpperCase());
+    }
+}
